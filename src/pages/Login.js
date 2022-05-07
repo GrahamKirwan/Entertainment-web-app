bfbdb157a4d20d2e5ed2e6b8d067b29dc72be5b0
Graham Kirwan <graham.kirwan4@gmail.com>
2022-05-07 11:12:00 -0400

    login and signup logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 import {LoginContainerStyled, FormContainer} from '../components/styles/LoginContainerStyled'
 
@@ -6,17 +7,34 @@ import { ReactComponent as Logo } from '../images/assets/logo.svg'
 
 
 export default function Login() {
+
+    const [loginOrSignup, setLoginOrSignup] = useState('login');
+
+    function setLoginOrSignupHandler() {
+        if(loginOrSignup == 'login') {
+            setLoginOrSignup('signup');
+        }
+
+        if(loginOrSignup == 'signup') {
+            setLoginOrSignup('login');
+        }
+    }
+
+    function submitHandler() {
+
+    }
+
   return (
     <LoginContainerStyled>
         <Logo/>
         <FormContainer>
-            <h1>Sign up</h1>
-            <form>
+            {loginOrSignup == 'login' ? (<h1>Login</h1>) : (<h1>Sign Up</h1>)}
+            <form onSubmit={submitHandler}>
                 <input type="text" placeholder='Email Address'></input>
                 <input type="password" placeholder='Password'></input>
-                <input type="password" placeholder='Repeat Password'></input>
-                <button type='submit'>Create an Account</button>
-                <div><span>Don't have an account? </span><a>Login</a></div>
+                {loginOrSignup == 'signup' ? (<input type="password" placeholder='Repeat Password'></input>) : ''}
+                <button type='submit'>{loginOrSignup == 'login' ? ('Log in to your account') : ('Create an Account')}</button>
+                <div>{loginOrSignup == 'login' ? (<span>Don't have an account?</span>) : (<span>Already have an account?</span>)} {loginOrSignup == 'login' ? (<a onClick={setLoginOrSignupHandler}>Sign up</a>) : (<a onClick={setLoginOrSignupHandler}>Login</a>)}</div>
             </form>
         </FormContainer>
     </LoginContainerStyled>
